2725a160babce0e53cfad8b04a28198860358d4c
senyu <772948909@qq.com>
2018-04-14 20:19:06 +0800

    完成遍历全国省市县的基本逻辑和基本布局（初步）。

---
+++ app/src/main/java/com/coolweather/android/util/Utility.java
@@ -0,0 +1,81 @@
+package com.coolweather.android.util;
+
+import android.text.TextUtils;
+import android.widget.Toast;
+
+import com.coolweather.android.MainActivity;
+import com.coolweather.android.db.City;
+import com.coolweather.android.db.Country;
+import com.coolweather.android.db.Province;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by 森宇 on 2018/4/14.
+ * 用于解析和处理服务器返回的省市县的JSON数据
+ */
+
+public class Utility {
+
+    public static boolean handleProvinceResponse(String response){
+        if(!TextUtils.isEmpty(response)){
+            try{//先用JSON数组里的JSONObject来解析数据，然后组装成实体类对象，再save（）到数据库。
+                JSONArray allProvinces = new JSONArray(response);
+                for(int i=0; i<allProvinces.length(); i++){
+                    JSONObject provinceObject = allProvinces.getJSONObject(i);
+                    Province province = new Province();
+                    province.setProvinceName(provinceObject.getString("name"));
+                    province.setProvinceCode(provinceObject.getInt("id"));
+                    province.save();
+                }
+                return true;
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    public static boolean handleCityResponse(String response,int provinceId){
+        if(!TextUtils.isEmpty(response)){
+            try{
+                JSONArray allCities = new JSONArray(response);
+                for(int i=0; i<allCities.length();i++){
+                    JSONObject cityObject = allCities.getJSONObject(i);
+                    City city = new City();
+                    city.setCityName(cityObject.getString("name"));
+                    city.setCityCode(cityObject.getInt("id"));
+                    city.setProvinceId(provinceId);
+                    city.save();
+                }
+                return true;
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    public static boolean handleCountryResponse(String response,int cityId){
+        if(!TextUtils.isEmpty(response)){
+            try{
+                JSONArray allCountries = new JSONArray();
+                for(int i=0; i<allCountries.length();i++){
+                    JSONObject countryObject = allCountries.getJSONObject(i);
+                    Country country = new Country();
+                    country.setCountryName(countryObject.getString("name"));
+                    country.setWeatherId(countryObject.getString("weather_id"));
+                    country.setCityId(cityId);
+                    country.save();
+                }
+                return true;
+            }catch(JSONException e){
+
+            }
+        }
+
+        return false;
+    }
+}
